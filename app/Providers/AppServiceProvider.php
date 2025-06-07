98a134d0d1516ee559d6ac4dd9025720cbe4e2c1
mpruidze <medeapruidze3@gmail.com>
2025-06-08 01:43:56 +0400

    added ratelimiting and prevent lazu loading to appServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,38 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        Model::preventLazyLoading(! $this->app->environment('production'));
+
+        $this->configureRateLimiting();
+    }
+
+    protected function configureRateLimiting(): void
+    {
+        RateLimiter::for('api', static function (Request $request) {
+            $limit = $request->user()
+                ? Limit::perMinute(200)->by($request->user()->id)
+                : Limit::perMinute(60)->by($request->ip());
+
+            return $limit->response(function (Request $request, array $headers) {
+                return response()->json([
+                    'message' => __('auth.throttle', ['seconds' => 60]),
+                ], 429, $headers);
+            });
+        });
     }
 }
