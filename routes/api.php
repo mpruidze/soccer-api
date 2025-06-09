0b46a4f12037b733c87f2f60a438e818e37bc09a
mpruidze <medeapruidze3@gmail.com>
2025-06-10 03:11:29 +0400

    added route name

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ Route::group(['prefix' => 'auth'], function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
     Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('logout');
-    Route::get('/user', [AuthController::class, 'getUser'])->middleware('auth:sanctum');
+    Route::get('/user', [AuthController::class, 'getUser'])->middleware('auth:sanctum')->name('auth.user');
 });
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
